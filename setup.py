80eb61196d627e593ff2c83ed110079a2e13c3a5
Nino Walker <nino.walker@gmail.com>
2013-03-01 17:17:36 -0800

    Inject examples to readme.

--- setup.py
+++ setup.py
@@ -2,24 +2,41 @@ import os
 from setuptools import setup
 
 from marx import __version__
+import glob
+
+README = "README.md"
+
+base = os.path.dirname(__file__)
+local = lambda x: os.path.join(base, x)
 
 def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+    return open(local(fname)).read()
+
+def hydrate_examples():
+    examples = {}
+    for f in glob.glob(local('examples/*')):
+        examples[os.path.basename(f)] = "\n    ".join(read(f).split("\n"))
+    readme = read(README +".in") % examples
+    with open(local(README), "w") as f:
+        f.write(readme)
+
+hydrate_examples()
 
 setup(
-    name = "celerybus",
-    version = __version__,
-    author = "Nino Walker",
-    author_email = "nino.walker@gmail.com",
-    description = read("README.md").split("\n", 1)[0],
+    name="celerybus",
+    version=__version__,
+    author="Nino Walker",
+    author_email="nino.walker@gmail.com",
+    description=read(README).split("\n", 1)[0],
     url='https://github.com/ninowalker/marx',
-    license = "BSD",
+    license="BSD",
     packages=['marx'],
-    long_description=read('README.md'),
+    long_description=read(README),
     setup_requires=['nose>=1.0', 'coverage==3.6', 'nosexcover', 'mock'],
-    test_suite = 'nose.collector',
+    test_suite='nose.collector',
     classifiers=[
         "License :: OSI Approved :: BSD License",
     ],
-    #entry_points = {'console_scripts': ['']}
 )
+
+
